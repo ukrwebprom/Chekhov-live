9c31a0e13c1b5923de53007d6f9cf8e96d50c1fa
ukrwebprom <ukrwebprom@gmail.com>
2023-03-18 13:21:41 +0200

    upd

---
+++ src/components/BookEditor/BookEditor.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import { ContentElement } from './ContentElement';
+import { useState } from 'react';
+
+export const BookEditor = ({content}) => {
+    const [active, setActive] = useState(null)
+
+    return (
+        <ul>
+        {content.map(el => 
+        <li key={el.id}><ContentElement text={el.text}/>
+        </li>)}
+        </ul>
+        
+    )
+}
+
+BookEditor.propTypes = {
+    content: PropTypes.array
+}
